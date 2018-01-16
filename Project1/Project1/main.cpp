dbdd240ae74e836675e4d972f37ec1f022da5819
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2018-01-16 14:42:32 -0700

    initialize project folder

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Hsuan Lee on 1/16/18.
+//  Copyright © 2018 Hsuan Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
